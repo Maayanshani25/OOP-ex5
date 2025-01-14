bf8350c49d34076a2be19225c4ce7c329653cf65
rotem <rotem.garti@mail.huji.ac.il>
2025-01-14 15:09:40 +0200

    first draft with the required structure

---
+++ ex5/main/Sjavac.java
@@ -0,0 +1,19 @@
+package ex5.main;
+
+// TODO: should be in ex5 package?
+
+public class Sjavac {
+
+    public static void main(String[] args) {
+        String source_file_name = args[0];
+
+//        // TODO: if the code is legal:
+//        System.out.println(0);
+//        // TODO: if the code is illegal:
+//        System.out.println(1);
+//        System.err(Constants.ILLEGAL_CODE_MESSAGE);
+//        // TODO: in case of IO errors:
+//        System.out.println(2);
+//        System.err(Constants.IO_ERROR_MESSAGE);
+    }
+}
---
+++ ex5/util/Constants.java
@@ -0,0 +1,32 @@
+package ex5.util;
+
+/**
+ * A utility class to store commonly used constants throughout the project.
+ *
+ * <h2>How to Use</h2>
+ * <p>You can access the constants using the class name directly, as they are
+ * declared as <code>public static final</code>. Example usage:</p>
+ *
+ * <pre>
+ * // Access a string constant
+ * String example = Constants.EXAMPLE;
+ *
+ * // Use a numeric constant
+ * int doubledValue = Constants.NUM_EXAMPLE * 2;
+ *
+ * // Use a boolean constant
+ * if (Constants.DEBUG_MODE) {
+ *     System.out.println("Debug mode is enabled.");
+ * }
+ * </pre>
+ */
+
+
+public class Constants {
+    /** <h2> String constants </h2>*/
+    // Messeges:
+    public static final String
+        ILLEGAL_CODE_MESSAGE = "",
+        IO_ERROR_MESSAGE = "";
+
+}
