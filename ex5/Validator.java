cd19cfa83b3439a02a20eef51681f3d6b4690d91
rotem <rotem.garti@mail.huji.ac.il>
2025-01-17 17:55:17 +0200

    with FileReader, SymbolTable, ScopeManager: DONE

--- ex5/Validator.java
+++ ex5/Validator.java
@@ -1,6 +1,13 @@
 package ex5;
 
+import ex5.scope_managing.SymbolTable;
+
 public class Validator {
+    public Validator(SymbolTable symbolTable) {
+
+    }
+
+
     // TODO: from the instructions - should use the relevant Exceptions and not just true or false
     public static boolean isValidLine(String line) {
         // TODO: write this method
--- ex5/main/Sjavac.java
+++ ex5/main/Sjavac.java
@@ -1,10 +1,48 @@
 package ex5.main;
 
-public class Sjavac {
+import ex5.FileReader;
+import ex5.Validator;
+import ex5.scope_managing.SymbolTable;
 
-    public static void main(String[] args) {
-        String source_file_name = args[0];
+import java.io.IOException;
+import java.util.List;
+
+public class Sjavac {
+    private final List<String> preprocessedLines;
+    private final SymbolTable symbolTable;
+    // TODO: needed? or Validator methods are static?
+    private Validator validator;
+
+    public Sjavac(String filePath) {
+        // Read and pre-process the file:
+        List<String> allLines = FileReader.readLines(filePath);
+        preprocessedLines = FileReader.preProcessLines(allLines);
+
+        symbolTable = new SymbolTable();
+        validator = new Validator(symbolTable);
+    }
+
+    public boolean run() {
+        // TODO: i think this is wrong implement
+        // For every line, check if valid
+        for (String line : preprocessedLines) {
+            if (!Validator.isValidLine(line)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // TODO: make sure handling IOExceptions correctly
+    public static void main(String[] args) throws IOException {
         // TODO: if the args num is invalid, filename is wrong, or file is not sJava - throw IOException
+        //  (for now, being catch in the FileReader)
+        if (args.length != 1) {
+            throw new IOException();
+        }
+        String source_file_name = args[0];
+        Sjavac sjavac = new Sjavac(source_file_name);
+
 
 //        // TODO: if the code is legal:
 //        System.out.println(0);
